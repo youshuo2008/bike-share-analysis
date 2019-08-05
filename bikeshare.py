674d52851f2590fba3123f9cc48a41d999ae76eb
Shuo You <youshuo2008@gmail.com>
2019-08-05 08:10:41 -0400

    refractor: Add a print message for not to restart the program

--- bikeshare.py
+++ bikeshare.py
@@ -209,6 +209,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print('\nThank you. Hope you find this program useful. Have a wonderful day! ')
             break
 
 
